c16c70c6615c7567768c1b9faae0f344f4bf33dd
魏永涛 <weiyongtao@windward.com.cn>
2021-03-26 12:51:01 +0800

    ok

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
-// console.log(global);
-// console.log(__dirname);
-// console.log(__filename);
-// console.log('index');
-// console.log(process); // 有很多属性
-// console.log(process.env);
-console.log(process.cwd()); // 当前运行node目录
+// server端
+const net = require('net');
+const server = net.createServer();
+const fs = require('fs');
+const path = require('path');
+
+// 服务器监听88端口
+server.listen(88)
+
+server.on('listening', () => {
+    console.log('server listening 88');
+})
+
+server.on('connection', socket => {
+    console.log('有客户端连接到了服务器');
+    // 给客户端发送数据
+    socket.on('data', async chunk => {
+        // 数据
+        const filename = path.resolve(__dirname, './myfiles/test.jpg');
+        const bodyBuffer = await fs.promises.readFile(filename);
+        const headeBuffer = Buffer.from(`HTTP/1.1 200 OK
+Content-Type: image/jpeg
+
+`,
+        'utf-8')
+        const result = Buffer.concat([headeBuffer, bodyBuffer])
+        socket.write(result)
+        socket.end();
+    })
+    socket.on('end', () => {
+        console.log('连接关闭了');
+    })
+})
+
+
+
+
+
+
+
+
