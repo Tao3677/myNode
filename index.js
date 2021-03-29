b544ed62ff30167e4f1593e64be58a8e6a21f831
魏永涛 <weiyongtao@windward.com.cn>
2021-03-29 12:40:30 +0800

    暂存

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-// console.log(global);
-// console.log(__dirname);
-// console.log(__filename);
-// console.log('index');
-// console.log(process); // 有很多属性
-// console.log(process.env);
-console.log(process.cwd()); // 当前运行node目录
+const http = require('http');
+const request = http.request(
+    'http://yuanjin.tech:5005/api/movie',
+    {
+        method: 'GET'
+    },
+    resp => {
+        console.log('服务器响应状态码：', resp.statusCode);
+        console.log('服务器响应头：', resp.headers)
+        console.log('服务器响应头-content-type：', resp.headers['content-type'])
+        resp.on('data', chunk => {
+            console.log(JSON.parse(chunk.toString('utf-8')));
+        })
+        resp.on('end', chunk => {
+            console.log('end');
+        })
+    }
+)
+request.end();
+
+
+
+
+
+
+
+
+
+
+
+
+
